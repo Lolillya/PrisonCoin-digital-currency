08f408d2f5037c69c676cd230bdbebdd40cf9acb
Lolillya <someonesusernam@gmail.com>
2025-05-21 15:33:00 +0800

    1 - ADD: initial setup for smart contracts and client-side

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <section className="bg-black text-white">
+        <span>hello world</span>
+      </section>
+    </>
+  );
+}
+
+export default App;
