b7218e8cc45b0d73bf5ec589904f118d5b6b974f
Lolillya <someonesusernam@gmail.com>
2025-06-04 01:40:47 +0800

    added register page screens

--- client/src/pages/admin/register/components/register-inputs.tsx
+++ client/src/pages/admin/register/components/register-inputs.tsx
@@ -1,116 +1,205 @@
+import { FingerprintIcon, LeftArrowIcon } from "@/components/icons/icons";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { Modal } from "@/components/ui/modal";
 import { useRegisterSteps } from "@/hooks/register-page/register-page-steps.query";
+import { useState } from "react";
 
 export const RegisterInputs = () => {
-  const { updateStep } = useRegisterSteps();
+  const { updateStep, steps } = useRegisterSteps();
 
-  const handleContinue = (e: React.FormEvent<HTMLFormElement>) => {
-    updateStep(1); // This will mark step 1 as complete
-
+  const handleContinue = (
+    e: React.MouseEvent<HTMLButtonElement>,
+    stepId: number
+  ) => {
     e.preventDefault();
-    console.log("Continue button clicked");
+    updateStep(stepId);
   };
 
+  console.log("Step completed:", steps);
   return (
-    <form onSubmit={handleContinue}>
+    <form className="flex-1 flex h-full">
       {/* STEP 1 */}
-      {/* <div className="w-full h-full flex m-auto">
-        <div className="flex flex-col gap-4 w-full">
-          <div className="flex flex-col relative group">
+      {/* {!steps.step1 && <RegisterStep1 onContinue={(e) => handleContinue(e, 1)} /> } */}
+      {/* STEP 2 */}
+      {/* {!(!steps.step1 && !steps.step1) && <RegisterStep2 onContinue={(e) => handleContinue(e, 2)} />} */}
+      <RegisterStep3 onContinue={(e) => handleContinue(e, 3)} />
+    </form>
+  );
+};
+
+const RegisterStep1 = ({
+  onContinue,
+}: {
+  onContinue: (e: React.MouseEvent<HTMLButtonElement>) => void;
+}) => {
+  return (
+    <div className="w-full h-full flex m-auto">
+      <div className="flex flex-col gap-4 w-full">
+        <div className="w-full flex justify-between items-center">
+          <div className="p-4 bg-primary text-text rounded-lg flex items-center gap-1">
+            <LeftArrowIcon />
+            <span>Back</span>
+          </div>
+          <h2>Personal Information</h2>
+        </div>
+        <div className="flex flex-col relative group">
+          <label className="absolute ml-4 text-secondary/70 text-base mt-2 transition-all duration-200 group-focus-within:text-xs">
+            Full Name
+          </label>
+          <Input placeholder="John Doe" required />
+        </div>
+
+        <div className="flex flex-col relative group">
+          <label className="absolute ml-4 text-secondary/70 text-base mt-2 transition-all duration-200 group-focus-within:text-xs">
+            In-mate No. (auto-generated)
+          </label>
+          <Input placeholder="#12345" required />
+        </div>
+
+        <div className="flex flex-col relative group">
+          <label className="absolute ml-4 text-secondary/70 text-base mt-2 transition-all duration-200 group-focus-within:text-xs">
+            Address
+          </label>
+          <Input placeholder="Full Address" required />
+        </div>
+
+        <div className="flex gap-4 w-full">
+          <div className="flex flex-col relative group w-full">
             <label className="absolute ml-4 text-secondary/70 text-base mt-2 transition-all duration-200 group-focus-within:text-xs">
-              Full Name
+              Height
             </label>
-            <Input placeholder="John Doe" required/>
+            <Input placeholder="in inches" required />
           </div>
 
-          <div className="flex flex-col relative group">
+          <div className="flex flex-col relative group w-full">
             <label className="absolute ml-4 text-secondary/70 text-base mt-2 transition-all duration-200 group-focus-within:text-xs">
-              In-mate No. (auto-generated)
+              Weight
             </label>
-            <Input placeholder="#12345" required/>
-          </div>
-
-          <div className="flex flex-col relative group">
-            <label className="absolute ml-4 text-secondary/70 text-base mt-2 transition-all duration-200 group-focus-within:text-xs">
-              Address
-            </label>
-            <Input placeholder="Full Address" required/>
-          </div>
-
-          <div className="flex gap-4 w-full">
-            <div className="flex flex-col relative group w-full">
-              <label className="absolute ml-4 text-secondary/70 text-base mt-2 transition-all duration-200 group-focus-within:text-xs">
-                Height
-              </label>
-              <Input placeholder="in inches" required/>
-            </div>
-
-            <div className="flex flex-col relative group w-full">
-              <label className="absolute ml-4 text-secondary/70 text-base mt-2 transition-all duration-200 group-focus-within:text-xs">
-                Weight
-              </label>
-              <Input placeholder="in kg" required/>
-            </div>
-          </div>
-          <div>
-            <Button type="submit">Continue</Button>
+            <Input placeholder="in kg" required />
           </div>
         </div>
-      </div> */}
+        <div>
+          <Button type="submit" onClick={onContinue}>
+            Continue
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-      {/* STEP 2 */}
-      <div className="w-full h-full flex m-auto">
-        <div className="flex flex-col gap-4 w-full">
+const RegisterStep2 = ({
+  onContinue,
+}: {
+  onContinue: (e: React.MouseEvent<HTMLButtonElement>) => void;
+}) => {
+  return (
+    <div className="w-full h-full flex m-auto">
+      <div className="flex flex-col gap-4 w-full">
+        <div className="w-full flex justify-between items-center">
+          <div className="p-4 bg-primary text-text rounded-lg flex items-center gap-1">
+            <LeftArrowIcon />
+            <span>Back</span>
+          </div>
           <h2>Arrest Information</h2>
-          <div className="flex flex-col relative group">
+        </div>
+        <div className="flex flex-col relative group">
+          <label className="absolute ml-4 text-secondary/70 text-base mt-2 transition-all duration-200 group-focus-within:text-xs">
+            Arresting Officer
+          </label>
+          <Input placeholder="John Doe" required />
+        </div>
+
+        <div className="flex gap-4 items-center w-full">
+          <div className="group relative w-full">
             <label className="absolute ml-4 text-secondary/70 text-base mt-2 transition-all duration-200 group-focus-within:text-xs">
-              Arresting Officer
+              Arrest Date
             </label>
-            <Input placeholder="John Doe" required />
+            <Input type="date" className="w-full" required />
           </div>
 
-          <div className="flex gap-4 items-center w-full">
-            <div className="group relative w-full">
-              <label className="absolute ml-4 text-secondary/70 text-base mt-2 transition-all duration-200 group-focus-within:text-xs">
-                Arrest Date
-              </label>
-              <Input type="date" className="w-full" required />
+          <div className="group relative w-full">
+            <label className="absolute ml-4 text-secondary/70 text-base mt-2 transition-all duration-200 group-focus-within:text-xs">
+              Time of Arrest
+            </label>
+            <Input type="time" className="w-full" required />
+          </div>
+        </div>
+
+        <div className="flex flex-col relative group">
+          <label className="absolute ml-4 text-secondary/70 text-base mt-2 transition-all duration-200 group-focus-within:text-xs">
+            Location of Arrest
+          </label>
+          <Input placeholder="John Doe" required />
+        </div>
+
+        <div className="flex flex-col relative group">
+          <label className="absolute ml-4 text-secondary/70 text-base mt-2 transition-all duration-200 group-focus-within:text-xs">
+            Charges / Description of Crime
+          </label>
+          <Input placeholder="John Doe" required />
+        </div>
+
+        <div className="flex flex-col relative group">
+          <label className="absolute ml-4 text-secondary/70 text-base mt-2 transition-all duration-200 group-focus-within:text-xs">
+            Image
+          </label>
+          <Input type="file" />
+        </div>
+        <div>
+          <Button type="submit" onClick={onContinue}>
+            Continue
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const RegisterStep3 = ({
+  onContinue,
+}: {
+  onContinue: (e: React.MouseEvent<HTMLButtonElement>) => void;
+}) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isScanComplete, setIsScanComplete] = useState(true);
+  return (
+    <>
+      {isModalOpen && <Modal setIsModalOpen={setIsModalOpen} />}
+      <div className="flex-1 flex flex-col py-5">
+        <div className="flex items-center justify-between">
+          <div className="p-4 bg-primary text-white rounded-lg flex items-center gap-1">
+            <LeftArrowIcon />
+            <span>Back</span>
+          </div>
+          <h2>Biometric Entrollment</h2>
+        </div>
+
+        <div className="flex flex-col gap-4 w-full flex-1 justify-between pt-5">
+          <div className="flex flex-col items-center justify-center gap-4">
+            <div className="text-white relative group bg-primary flex w-fit p-4 rounded-full shadow-lg">
+              <FingerprintIcon width={200} height={200} />
             </div>
-
-            <div className="group relative w-full">
-              <label className="absolute ml-4 text-secondary/70 text-base mt-2 transition-all duration-200 group-focus-within:text-xs">
-                Time of Arrest
-              </label>
-              <Input type="time" className="w-full" required />
-            </div>
+            <h3>Register Inmate fingerprint</h3>
+            <p className="text-center text-sm text-text/70 max-w-md mt-2">
+              This step securely links the inmate's identity to their digital
+              profile using a biometric fingerprint scan. The fingerprint will
+              be encrypted and stored for future authentication during purchases
+              and transactions.
+            </p>
           </div>
 
-          <div className="flex flex-col relative group">
-            <label className="absolute ml-4 text-secondary/70 text-base mt-2 transition-all duration-200 group-focus-within:text-xs">
-              Location of Arrest
-            </label>
-            <Input placeholder="John Doe" required />
-          </div>
-
-          <div className="flex flex-col relative group">
-            <label className="absolute ml-4 text-secondary/70 text-base mt-2 transition-all duration-200 group-focus-within:text-xs">
-              Charges / Description of Crime
-            </label>
-            <Input placeholder="John Doe" required />
-          </div>
-
-          <div className="flex flex-col relative group">
-            <label className="absolute ml-4 text-secondary/70 text-base mt-2 transition-all duration-200 group-focus-within:text-xs">
-              Image
-            </label>
-            <Input type="file" />
-          </div>
-          <div>
-            <Button type="submit">Continue</Button>
+          <div className="text-center flex flex-col">
+            <p className="text-sm text-text/70 mb-2">
+              Start the scanning process below.
+            </p>
+            <Button onClick={() => setIsModalOpen(!isModalOpen)}>
+              {isScanComplete ? "Continue" : "Scan Fingerprint"}
+            </Button>
           </div>
         </div>
       </div>
-    </form>
+    </>
   );
 };
--- client/src/pages/admin/register/components/register-steps.tsx
+++ client/src/pages/admin/register/components/register-steps.tsx
@@ -1,20 +1,7 @@
-import React from "react";
+
 
 export const RegisterSteps = () => {
-  const [isStep1Complete, setIsStep1Complete] = React.useState(false);
-  const [isStep2Complete, setIsStep2Complete] = React.useState(false);
-  const [isStep3Complete, setIsStep3Complete] = React.useState(false);
-  const [isStep4Complete, setIsStep4Complete] = React.useState(false);
-  const [isStep5Complete, setIsStep5Complete] = React.useState(false);
-
-  const [currentStep, setCurrentStep] = React.useState(1);
-  const steps = [
-    { label: "Register Label 1", isComplete: isStep1Complete },
-    { label: "Register Label 2", isComplete: isStep2Complete },
-    { label: "Register Label 3", isComplete: isStep3Complete },
-    { label: "Register Label 4", isComplete: isStep4Complete },
-    { label: "Register Label 5", isComplete: isStep5Complete },
-  ];
+  
 
   return (
     /* This is a simple stepper component that displays the steps of a registration process.
@@ -22,7 +9,7 @@ export const RegisterSteps = () => {
     <div className="flex items-end">
       {/* STEP 1 */}
       <div className="flex flex-col justify-center items-center gap-1">
-        <label className="text-xs text-center">Register Label</label>
+        <label className="text-xs text-center">Personal Information</label>
         <div className="flex justify-center items-center w-5 h-5 bg-primary rounded-full text-white p-5 font-bold">
           <span>1</span>
         </div>
@@ -33,7 +20,7 @@ export const RegisterSteps = () => {
 
       {/* STEP 2 */}
       <div className="flex flex-col justify-center items-center gap-1">
-        <label className="text-xs text-center">Register Label</label>
+        <label className="text-xs text-center">Arrest Information</label>
         <div className="flex justify-center items-center w-5 h-5 bg-gray-400 rounded-full text-white p-5 font-bold">
           <span>2</span>
         </div>
@@ -44,7 +31,7 @@ export const RegisterSteps = () => {
 
       {/* STEP 3 */}
       <div className="flex flex-col justify-center items-center gap-1">
-        <label className="text-xs text-center">Register Label</label>
+        <label className="text-xs text-center">Biometric Entrollment</label>
         <div className="flex justify-center items-center w-5 h-5 bg-gray-400 rounded-full text-white p-5 font-bold">
           <span>3</span>
         </div>
@@ -55,7 +42,7 @@ export const RegisterSteps = () => {
 
       {/* STEP 4 */}
       <div className="flex flex-col justify-center items-center gap-1">
-        <label className="text-xs text-center">Register Label</label>
+        <label className="text-xs text-center">Wallet & Token Assignment</label>
         <div className="flex justify-center items-center w-5 h-5 bg-gray-400 rounded-full text-white p-5 font-bold">
           <span>4</span>
         </div>
@@ -66,7 +53,7 @@ export const RegisterSteps = () => {
 
       {/* STEP 5 */}
       <div className="flex flex-col justify-center items-center gap-1">
-        <label className="text-xs text-center">Register Label</label>
+        <label className="text-xs text-center">Register Confirmation</label>
         <div className="flex justify-center items-center w-5 h-5 bg-gray-400 rounded-full text-white p-5 font-bold">
           <span>5</span>
         </div>
