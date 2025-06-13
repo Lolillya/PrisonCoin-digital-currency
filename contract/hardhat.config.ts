7943d82943a0e2a6ce30fa02e772be545515497c
Lolillya <someonesusernam@gmail.com>
2025-06-14 06:27:31 +0800

    added dependencies on contracts

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -3,6 +3,12 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  networks: {
+    localhost: {
+      url: "http://10.255.255.254:7545", // ✅ This is your Ganache host
+      chainId: 5777, // or 5777, depending on your Ganache settings
+    }
+  }
 };
 
 export default config;
